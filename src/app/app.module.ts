110828754ffac0ccab3ac977b3b486c864c6ad43
Pierre-D-G <gamil.pierre@gmail.com>
2017-07-04 11:33:07 -0400

    Created Contacts component which will show all a users contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+
+import { ContactsAPIService } from './services/contacts-api.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ContactsAPIService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
