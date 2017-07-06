314ba7780ed4b7b7d1d9f65f783f32ea0d31bdbc
Pierre-D-G <gamil.pierre@gmail.com>
2017-07-06 09:36:28 -0400

    API route to create a new contact

--- server/api/api.js
+++ server/api/api.js
@@ -40,7 +40,7 @@ router.get('/', (req, res) => {
   Contact.findAll().then(contacts => {
     return res.json(contacts)
   }).catch(err => {
-    return res.json({errors: ['Could not retrieve contacts']})
+    return res.json({ errors: ['Could not retrieve contacts'] })
   })
 });
 
@@ -54,8 +54,33 @@ router.get('/contact/:id', (req, res) => {
   }).then(contact => {
     return res.json(contact)
   }).catch(err => {
-    return res.json({errors: ['Could not retrieve contact from the database']})
+    return res.json({ errors: ['Could not retrieve contact from the database'] })
   })
 });
+
+// Add new Contact
+
+router.post('/contacts', (req, res) => {
+  Contact.create({
+    avatar_url: req.body.avatar_url,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    company: req.body.company,
+    home_phone: req.body.home_phone,
+    mobile_phone: req.body.mobile_phone,
+    work_phone: req.body.work_phone,
+    street_address: req.body.street_address,
+    city: req.body.city,
+    state: req.body.state,
+    zip_code: req.body.zip_code
+  }).then(function(){
+    res.status(201);
+    return res.json({"success": "Contact Created"})
+  }).catch(err => {
+    res.status(400);
+    return res.json({"error": "Bad Request" })
+  })
+})
 
 module.exports = router;
