f48c5c4e9d06d551c61db7b4230576235410447b
Pierre-D-G <gamil.pierre@gmail.com>
2017-07-08 20:54:04 -0400

    Refactored requests to API server and created Delete contact request to api server

--- src/app/services/contacts-api.service.ts
+++ src/app/services/contacts-api.service.ts
@@ -1,41 +1,61 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, URLSearchParams } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { Http, Headers, URLSearchParams, Response } from '@angular/http';
+import { Contact } from '../models/contact';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class ContactsAPIService {
 
-  private apiURL: string;
+  private apiURL = 'http://localhost:3000/api/contacts';
 
   constructor(private _http: Http) { }
 
-  getContacts = () => {
-    this.apiURL = 'http://localhost:3000/api/contacts';
+  getContacts(): Promise<Contact[]> {
     return this._http.get(this.apiURL)
-      .map(res => res.json())
+      .toPromise()
+      .then(response => response.json() as Contact[])
+      .catch(this.handleErrors)
   }
 
-  getContact = (id: string) => {
-    this.apiURL = 'http://localhost:3000/api/contacts/' + id;
-    return this._http.get(this.apiURL)
-      .map(res => res.json())
+  getContact(id: string): Promise<Contact[]> {
+    return this._http.get(this.apiURL + '/' + id)
+      .toPromise()
+      .then(response => response.json() as Contact[])
+      .catch(this.handleErrors)
   }
 
-  saveContact = (contact) => {
+  createContact(newContact: Contact): Promise<Contact> {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
 
-    let body = JSON.stringify(contact)
-    this.apiURL = 'http://localhost:3000/api/contacts';
+    let body = JSON.stringify(newContact);
     return this._http.post(this.apiURL, body, { headers: headers })
+      .toPromise()
+      .then(response => response.json() as Contact)
+      .catch(this.handleErrors)
   }
 
-  updateContact = (contact, id) => {
+  updateContact(putContact: Contact, id: string): Promise<Contact> {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
 
-    let body = JSON.stringify(contact)
-    this.apiURL = 'http://localhost:3000/api/contacts/' + id;
-    return this._http.post(this.apiURL, body, { headers: headers })
+    let body = JSON.stringify(putContact)
+    return this._http.put(this.apiURL + '/' + id, body, { headers: headers })
+      .toPromise()
+      .then(response => response.json() as Contact)
+      .catch(this.handleErrors)
+  }
+
+  deleteContact(id: String): Promise<String> {
+    return this._http.delete(this.apiURL + '/' + id)
+      .toPromise()
+      .then(response => response.json() as String)
+      .catch(this.handleErrors)
+  }
+
+  private handleErrors(error: any) {
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg);
   }
 }
